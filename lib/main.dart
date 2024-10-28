7c19ac19a9a891b24bdf3da9bb2cccf39ef3fd0c
eomgerm <fishbread00@gmail.com>
2024-10-28 18:41:25 +0900

    feat: bloc, getit 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:alda_front/alda_app.dart';
+import 'package:alda_front/injectable/configurations.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  configureDependencies();
   runApp(AlDaApp());
 }
